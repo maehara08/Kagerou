8ba8aeea035c84a13ce2de27fb8aead0f9aeee5e
Ranksai <ranks.j.inter1908@gmail.com>
2016-07-03 17:40:57 +0900

    add splash 5000 ms

--- app/src/main/java/com/developers/hack/cs/kagerou/activity/SplashActivity.java
+++ app/src/main/java/com/developers/hack/cs/kagerou/activity/SplashActivity.java
@@ -1,10 +1,12 @@
 package com.developers.hack.cs.kagerou.activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v4.content.IntentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import com.developers.hack.cs.kagerou.R;
@@ -16,10 +18,16 @@ public class SplashActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(this);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_splash);
+        Handler hdl = new Handler();
+
+        hdl.postDelayed(new splash(),5000);
+
+//        button = (Button)findViewById(R.id.button);
+//        button.setOnClickListener(this);
 
     }
 
@@ -28,6 +36,16 @@ public class SplashActivity extends AppCompatActivity implements View.OnClickLis
         if(v == button){
             Intent intent = new Intent(this,MainActivity.class);
                 startActivityForResult(intent,0);
+        }
+    }
+    class splash implements Runnable{
+        @Override
+        public void run() {
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+
+            startActivity(intent);
+
+            SplashActivity.this.finish();
         }
     }
 }

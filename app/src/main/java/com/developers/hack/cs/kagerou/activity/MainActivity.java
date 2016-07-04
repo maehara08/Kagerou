6c56e4e3e1aa77c018163db8a22a4ca75670d12d
maehara08 <aso.riiiku08@gmail.com>
2016-07-04 17:42:42 +0900

    fix permisson check

--- app/src/main/java/com/developers/hack/cs/kagerou/activity/MainActivity.java
+++ app/src/main/java/com/developers/hack/cs/kagerou/activity/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.developers.hack.cs.kagerou.R;
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private static final int REQUEST_PERMISSION = 10;
+    private static final int REQUEST_CODE = 1;
     private LocationManager LocationManager;
 
     TextView textView_x;
@@ -30,67 +33,95 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         textView_x = (TextView) findViewById(R.id.textView_x);
         textView_y = (TextView) findViewById(R.id.textView_y);
+        LocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        if (CheckPermission()) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION);
+
         } else {
-            locationStart();
+            if (isGpsEnable()) locationStart();
         }
-        CheckPermission();
-        locationStart();
     }
 
     protected void locationStart() {
-        LocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        final boolean gpsEnabled = LocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED
+//                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION);
+//
+//            return;
+//        }
+        Log.d(TAG, "LocationManagerStart");
 
-        if (!gpsEnabled) {
-            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(settingsIntent);
+        LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0.01f, this);
+        Location location = LocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            textView_x.setText("Latitude" + location.getLatitude());
+            textView_y.setText("Logitude" + location.getLongitude());
         }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
-
-            return;
-        }
-        LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
     }
 
-    protected void CheckPermission() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
-        } else {
-            locationStart();
+    protected boolean isGpsEnable() {
+        final boolean gpsEnabled = LocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(settingsIntent, REQUEST_CODE);
         }
+        return gpsEnabled;
+    }
+
+    protected boolean CheckPermission() {
+        return (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        locationStart();
     }
 
     @Override
     public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged");
         textView_x.setText("Latitude" + location.getLatitude());
         textView_y.setText("Logitude" + location.getLongitude());
-
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d(TAG, "onStatusChanged");
+        TextView textView = (TextView) (findViewById(R.id.provider));
+        TextView textView1 = (TextView) findViewById(R.id.status);
+        textView.setText(provider);
+        switch (status) {
+            case LocationProvider.AVAILABLE:
+                textView1.setText(provider + "が利用できます");
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                textView1.setText(provider + "が圏外で利用できません");
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                textView1.setText("一時的に利用できません");
+                break;
+        }
 
     }
 
     @Override
     public void onProviderEnabled(String provider) {
+        Log.d(TAG, "onProviderEnabled");
 
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-
+        Log.d(TAG, "onProviderDisabled");
     }
 }

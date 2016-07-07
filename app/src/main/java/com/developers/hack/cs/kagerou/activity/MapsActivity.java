740e783a7eb5457cdb0c12a0b7b1afae6be186a0
maehara08 <aso.riiiku08@gmail.com>
2016-07-07 16:06:30 +0900

    format

--- app/src/main/java/com/developers/hack/cs/kagerou/activity/MapsActivity.java
+++ app/src/main/java/com/developers/hack/cs/kagerou/activity/MapsActivity.java
@@ -39,7 +39,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private static final String TAG = MapsActivity.class.getSimpleName();
 
-    private static final int REQUEST_PERMISSION=3;
+    private static final int REQUEST_PERMISSION = 3;
 
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
@@ -65,7 +65,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-
         mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
@@ -79,6 +78,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         fusedLocationProviderApi = LocationServices.FusedLocationApi;
     }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -126,6 +126,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.d(TAG, String.valueOf(mResolvingError));
         }
     }
+
     private void stopFusedLocation() {
         // Disconnecting the client invalidates it.
         mGoogleApiClient.disconnect();
@@ -133,15 +134,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera. In this case,
-         * we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to install
-         * it inside the SupportMapFragment. This method will only be triggered once the user has
-         * installed Google Play services and returned to the app.
-         */
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -160,7 +161,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .fillColor(0xaa2f7b8e);
         circle = mMap.addCircle(circleOptions);
     }
-
 
 
     @Override
@@ -188,7 +188,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             textLog += "Time=" + String.valueOf(location.getTime()) + "\n";
             textLog += "Speed=" + String.valueOf(location.getSpeed()) + "\n";
             textLog += "Bearing=" + String.valueOf(location.getBearing()) + "\n";
-            mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude())).title("Now"));
+            mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Now"));
 
             Log.d(TAG, textLog);
         } else {
@@ -235,7 +235,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         textLog += "Speed=" + String.valueOf(location.getSpeed()) + "\n";
         textLog += "Bearing=" + String.valueOf(location.getBearing()) + "\n";
         textLog += "time= " + String.valueOf(lastLocationTime) + " msec \n";
-        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude())).title("Now"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Now"));
 
         Log.d(TAG, "onLocationChanged: " + textLog);
     }

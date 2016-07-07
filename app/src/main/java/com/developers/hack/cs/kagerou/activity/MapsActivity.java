c4ebbc341bcab8e24b72de696fcb1665cf27cd38
maehara08 <aso.riiiku08@gmail.com>
2016-07-07 16:05:01 +0900

    add gps

--- app/src/main/java/com/developers/hack/cs/kagerou/activity/MapsActivity.java
+++ app/src/main/java/com/developers/hack/cs/kagerou/activity/MapsActivity.java
@@ -1,16 +1,24 @@
 package com.developers.hack.cs.kagerou.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.developers.hack.cs.kagerou.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,17 +31,30 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
 
-    private static String TAG = "tag";
+    private static final String TAG = MapsActivity.class.getSimpleName();
+
+    private static final int REQUEST_PERMISSION=3;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderApi fusedLocationProviderApi;
+    private boolean mResolvingError = false;
+    private Location location;
+    private long lastLocationTime;
+    private LatLng mNowLatLng;
 
     private GoogleMap mMap;
     Circle circle;
 
     double lat = 35.6585805;
     double lng = 139.7454329;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,36 +64,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setFastestInterval(16);
+
+        fusedLocationProviderApi = LocationServices.FusedLocationApi;
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Tokyo Tower and move the camera
-        LatLng tokyo_tower = new LatLng(lat, lng);
-        mMap.addMarker(new MarkerOptions().position(tokyo_tower).title("Tokyo Tower"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(tokyo_tower));
-
-        //circle
-        final CircleOptions circleOptions = new CircleOptions()
-                .center(new LatLng(lat, lng))
-                .radius(100)
-                .strokeWidth(5)
-                .strokeColor(0xe1285577)
-                .fillColor(0xaa2f7b8e);
-        circle = mMap.addCircle(circleOptions);
-    }
-
     @Override
     public void onResume() {
         super.onResume();
@@ -99,22 +105,154 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    protected void onStart() {
+        super.onStart();
+        startFusedLocation();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopFusedLocation();
+    }
+
+    private void startFusedLocation() {
+        Log.d(TAG, "startFused");
+        //Connect the Client
+        if (!mResolvingError) {
+            //Connect the Client
+            mGoogleApiClient.connect();
+        } else {
+            Log.d(TAG, String.valueOf(mResolvingError));
+        }
+    }
+    private void stopFusedLocation() {
+        // Disconnecting the client invalidates it.
+        mGoogleApiClient.disconnect();
+        Log.d(TAG, "stopFusedLocation");
+    }
+
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera. In this case,
+         * we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to install
+         * it inside the SupportMapFragment. This method will only be triggered once the user has
+         * installed Google Play services and returned to the app.
+         */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Tokyo Tower and move the camera
+        LatLng tokyo_tower = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(tokyo_tower).title("Tokyo Tower"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tokyo_tower));
+
+        //circle
+        final CircleOptions circleOptions = new CircleOptions()
+                .center(new LatLng(lat, lng))
+                .radius(100)
+                .strokeWidth(5)
+                .strokeColor(0xe1285577)
+                .fillColor(0xaa2f7b8e);
+        circle = mMap.addCircle(circleOptions);
+    }
+
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onConnected: return");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION);
+
+            return;
+        }
+        Location fusedLocation = fusedLocationProviderApi.getLastLocation(mGoogleApiClient);
+
+        if (fusedLocation != null && fusedLocation.getTime() > 20000) {
+            location = fusedLocation;
+            String textLog = "";
+            textLog += "---------- onConnected \n";
+            textLog += "Latitude=" + String.valueOf(location.getLatitude()) + "\n";
+            textLog += "Longitude=" + String.valueOf(location.getLongitude()) + "\n";
+            textLog += "Accuracy=" + String.valueOf(location.getAccuracy()) + "\n";
+            textLog += "Altitude=" + String.valueOf(location.getAltitude()) + "\n";
+            textLog += "Time=" + String.valueOf(location.getTime()) + "\n";
+            textLog += "Speed=" + String.valueOf(location.getSpeed()) + "\n";
+            textLog += "Bearing=" + String.valueOf(location.getBearing()) + "\n";
+            mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude())).title("Now"));
+
+            Log.d(TAG, textLog);
+        } else {
+            //バックグラウンドから戻ると例外発生することがある
+            try {
+                //
+                fusedLocationProviderApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+                // Schedule a Thread to unregister location listeners
+                Executors.newScheduledThreadPool(1).schedule(new Runnable() {
+                    @Override
+                    public void run() {
+                        fusedLocationProviderApi.removeLocationUpdates(mGoogleApiClient, MapsActivity.this);
+                    }
+                }, 60000, TimeUnit.MILLISECONDS);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast toast = Toast.makeText(this, "例外が発生、位置情報のPermissionを許可していますか？", Toast.LENGTH_SHORT);
+                toast.show();
+
+                //MainActivityに戻す
+                finish();
+            }
+        }
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Log.d(TAG, "onConnectionSuspended");
     }
 
     @Override
     public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged");
+        lastLocationTime = location.getTime() - lastLocationTime;
+        String textLog = "";
+        textLog += "---------- onLocationChanged \n";
+        textLog += "Latitude=" + String.valueOf(location.getLatitude()) + "\n";
+        textLog += "Longitude=" + String.valueOf(location.getLongitude()) + "\n";
+        textLog += "Accuracy=" + String.valueOf(location.getAccuracy()) + "\n";
+        textLog += "Altitude=" + String.valueOf(location.getAltitude()) + "\n";
+        textLog += "Time=" + String.valueOf(location.getTime()) + "\n";
+        textLog += "Speed=" + String.valueOf(location.getSpeed()) + "\n";
+        textLog += "Bearing=" + String.valueOf(location.getBearing()) + "\n";
+        textLog += "time= " + String.valueOf(lastLocationTime) + " msec \n";
+        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude())).title("Now"));
 
+        Log.d(TAG, "onLocationChanged: " + textLog);
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed");
 
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            Log.d("", "Already attempting to resolve an error");
+
+            return;
+        } else if (connectionResult.hasResolution()) {
+
+        } else {
+            mResolvingError = true;
+        }
     }
 }

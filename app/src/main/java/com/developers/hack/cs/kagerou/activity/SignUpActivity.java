85bc812643fe33cfaeec4d1435495375a9b2653d
Ranksai <ranks.j.inter1908@gmail.com>
2016-07-31 16:35:50 +0900

    add sign_up_api

--- app/src/main/java/com/developers/hack/cs/kagerou/activity/SignUpActivity.java
+++ app/src/main/java/com/developers/hack/cs/kagerou/activity/SignUpActivity.java
@@ -10,8 +10,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.developers.hack.cs.kagerou.R;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class SignUpActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
@@ -21,7 +32,6 @@ public class SignUpActivity extends AppCompatActivity implements RadioGroup.OnCh
     private EditText password_editText;
     private EditText age_editText;
     private RadioGroup radioGroup;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,4 +66,58 @@ public class SignUpActivity extends AppCompatActivity implements RadioGroup.OnCh
             Log.d(TAG, "not select");
         }
     }
+
+    private void postSignUp(){
+        OkHttpClient client = new OkHttpClient();
+        final String userName = userName_editText.getText().toString();
+        final String password = password_editText.getText().toString();
+        final String age = age_editText.getText().toString();
+        int checkedId = radioGroup.getCheckedRadioButtonId();
+        final String gender = ((RadioButton)findViewById(checkedId)).getText().toString();
+
+        if(userName == null || password == null || age == null || checkedId == -1){
+            return;
+        }
+
+        String result = null;
+        RequestBody formbody = new FormBody.Builder()
+                .add("name",userName)
+                .add("password",password)
+                .add("age",age)
+                .add("gender",gender)
+                .build();
+        Request request = new Request.Builder()
+                .url(getString(R.string.endpoint)+"/login/signup")
+                .post(formbody)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    Intent intent = new Intent(getApplication(),BaseActivity.class);
+                    startActivity(intent);
+                    finish();
+                    showToast("SignUp 成功");
+                }else{
+                    showToast("SignUp 失敗");
+                }
+                response.close();
+            }
+        });
+    }
+
+    private void showToast(final String message){
+        runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        );
+    };
 }
